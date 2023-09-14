a0406be6b46d93e92b171704e4707d0481869ca0
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-09-14 12:56:37 -0600

    Sesión 14 - 14/09/2023

--- listas.c
+++ listas.c
@@ -5,6 +5,12 @@
 #define cls system("cls")
 #define NL putchar('\n')
 
+// Prototipos
+void insert( char dato );
+void insert1( char dato );
+void insertn( char dato, int pos );
+char remueve();
+
 //  Definicion del nodo
 struct Nodo 
 { 
@@ -60,6 +66,129 @@ void insert1( char dato )
 	return;
 }
 
+
+
+void insertn( char dato, int pos )
+{
+	int cont;
+	nodo *nuevo = NULL;
+	nuevo = ( nodo *) malloc( sizeof( nodo ) );
+	if( nuevo == NULL)     ///  _No hay memoria
+	{
+		printf( "No hay memoria disponible\n");
+		pausa;
+		exit(1);
+	}
+	nuevo -> info = dato;
+	nuevo -> sig = NULL;
+	if( raiz == NULL )
+	{
+		raiz = nuevo;
+	}
+	if( pos == 1)
+	{
+		nuevo ->sig = raiz;
+		raiz = nuevo;
+	}
+	else 
+	{
+		nodo *recorre = raiz;
+		cont = 1;
+		while( recorre->sig != NULL && cont < pos - 1)
+		{
+			recorre = recorre -> sig;
+			cont++;
+		}
+		nuevo ->sig = recorre ->sig;
+		recorre -> sig = nuevo;
+
+	}
+	return;
+}
+char remueve()
+{
+	char dato;
+	nodo *anterior = NULL, *siguiente = NULL;
+	if( raiz == NULL)
+	{
+		printf("Lista vacia, no hay nada que elimniar.\n");
+		pausa;
+		return '\0';
+	}
+	anterior = raiz;
+	if( anterior->sig == NULL)
+	{
+		raiz = NULL;
+		dato = anterior->info;
+		free(anterior);
+		return dato;
+	}
+	else
+	{
+		siguiente = anterior ->sig;
+		while(siguiente -> sig != NULL)
+		{
+			anterior = anterior->sig;			// anterior = siguiente;
+			siguiente = siguiente->sig;
+		}
+		dato = siguiente ->info;
+		anterior ->sig = NULL;
+		free(siguiente);
+		return dato;
+	}
+}
+
+char remueve1()
+{
+	char dato;
+	nodo *anterior = NULL;
+	if( raiz == NULL)
+	{
+		printf("Lista vacia, no hay nada que elimniar.\n");
+		pausa;
+		return '\0';
+	}
+	anterior = raiz;
+	raiz = raiz ->sig;
+	dato = anterior->info;
+	free(anterior);
+	return dato;
+}
+
+char remuevn(int pos)
+{
+	char dato;
+	nodo *anterior = NULL, *siguiente = NULL;
+	if( raiz == NULL)
+	{
+		printf("Lista vacia, no hay nada que elimniar.\n");
+		pausa;
+		return '\0';
+	}
+	anterior = raiz;
+	if( anterior->sig == NULL)
+	{
+		raiz = NULL;
+		dato = anterior->info;
+		free(anterior);
+		return dato;
+	}
+	else
+	{
+		siguiente = anterior ->sig;
+		while(siguiente -> sig != NULL)
+		{
+			anterior = anterior->sig;			// anterior = siguiente;
+			siguiente = siguiente->sig;
+		}
+		dato = siguiente ->info;
+		anterior ->sig = NULL;
+		free(siguiente);
+		return dato;
+	}
+}
+
+
 void imprimeLista()
 {
 	nodo *recorre = raiz;
@@ -84,6 +213,11 @@ int main()
 	 insert1('r');
 	 insert1('t');
 	 insert1('o');
+
+	 imprimeLista();
+
+	 printf("Salió un %c\n", remueve()  );
+	 printf("Salió un %c\n", remueve1()  );
 
 	 imprimeLista();
 
